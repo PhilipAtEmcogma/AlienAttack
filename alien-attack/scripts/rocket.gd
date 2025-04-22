3fee3ff4d64718c335c139fca60f00aa842e96ea
PhilipAtEmcogma <emcogma@gmail.com>
2025-04-22 18:35:10 +1200

    finished rocket script and scene. make enemy scene and movement

--- alien-attack/scripts/rocket.gd
+++ alien-attack/scripts/rocket.gd
@@ -4,6 +4,12 @@ extends Area2D
 # instead of needing to go into the script everytime.
 @export var speed = 200 
 
+@onready var visible_notifier = $VisibleNotifier
+
+func _ready():
+	#use connect to connect signal to a variable
+	visible_notifier.connect("screen_exited", _on_screen_exited)
+
 func _physics_process(delta: float):
 	# one problem with the calculating the speed below, is the actual speed of the
 	# rocket is determined by the frame rate the programe runs on.  i.e. if framrate of is 60,
@@ -14,4 +20,6 @@ func _physics_process(delta: float):
 	#below is the proper rate to make the speed independt of the frame rate, so speed is consistent
 	# across different framerate.  NOTE: delta means delta time, which meant how long has in time it passed.
 	global_position.x += speed*delta
-	
+
+func _on_screen_exited():
+	queue_free() #delete when rocket exit screen, Godot know it is exited when the VisibleNotifier node exit scren	
